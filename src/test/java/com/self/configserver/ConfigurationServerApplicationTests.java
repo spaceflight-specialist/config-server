2987810079600b8bac5e13d6ca4e3250ce7d175b
Muhammad Usman <muhammad.u@royalcyber.com>
2022-08-25 12:03:23 +0500

    init commit

---
+++ src/test/java/com/self/configserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
